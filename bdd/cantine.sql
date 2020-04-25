a0dd6d25140c16b2df71766f9c17aa867d6580b0
Loïc <loicguo@gmail.com>
2020-04-25 16:06:51 +0200

    Ajout les doc, exportation SQL et du shéma relationnel

--- bdd/cantine.sql
+++ bdd/cantine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 01 avr. 2020 à 09:23
+-- Généré le :  sam. 25 avr. 2020 à 14:05
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -44,6 +44,57 @@ CREATE TABLE IF NOT EXISTS `compte` (
 
 INSERT INTO `compte` (`id`, `identifiant`, `mdp`) VALUES
 (1, 'test', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiant`
+--
+
+DROP TABLE IF EXISTS `etudiant`;
+CREATE TABLE IF NOT EXISTS `etudiant` (
+  `nom` varchar(40) COLLATE utf8_bin NOT NULL,
+  `prenom` varchar(40) COLLATE utf8_bin NOT NULL,
+  `classe` varchar(20) COLLATE utf8_bin NOT NULL,
+  `cantine` varchar(40) COLLATE utf8_bin NOT NULL,
+  `jour` varchar(40) COLLATE utf8_bin NOT NULL,
+  `regime` text COLLATE utf8_bin NOT NULL,
+  `tarif` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `etudiant`
+--
+
+INSERT INTO `etudiant` (`nom`, `prenom`, `classe`, `cantine`, `jour`, `regime`, `tarif`) VALUES
+('test', 'test', 'Première', 'Oui', 'Me J V ', 'test', 72),
+('b', 'b', 'Terminale', 'Oui', 'Ma Me ', 'b', 48),
+('Goncalves', 'Nathan', 'BTS', 'Non', '', 'Non', 0),
+('Bruno', 'Marco', 'Terminale', 'Oui', 'L Ma ', 'bla', 48);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `professeur`
+--
+
+DROP TABLE IF EXISTS `professeur`;
+CREATE TABLE IF NOT EXISTS `professeur` (
+  `nom` varchar(40) COLLATE utf8_bin NOT NULL,
+  `prenom` varchar(40) COLLATE utf8_bin NOT NULL,
+  `cantine` varchar(40) COLLATE utf8_bin NOT NULL,
+  `jour` varchar(40) COLLATE utf8_bin NOT NULL,
+  `regime` text COLLATE utf8_bin NOT NULL,
+  `tarif` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `professeur`
+--
+
+INSERT INTO `professeur` (`nom`, `prenom`, `cantine`, `jour`, `regime`, `tarif`) VALUES
+('test', 'test', 'Oui', 'L Ma Me J V', 'Non', 120),
+('test2', 'test2', 'Oui', 'L Ma', 'Non', 48);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
